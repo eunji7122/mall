ff5bc5916543669e7c83ced57b0dec9c450a6571
eunji7122 <lej7122@gmail.com>
2024-03-25 23:14:00 +0900

    Section1: React-Router

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,47 +1,40 @@
-import { useCallback } from "react";
-import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import React from 'react';
+import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom";
 
-const ReadPage = () => {
+function ReadPage(props) {
+    const navigate = useNavigate()
+    const {tno} = useParams()
 
-  const {tno} = useParams()
+    const [queryParams] = useSearchParams()
 
-  const navigate = useNavigate()
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
 
-  const [queryParams] = useSearchParams()
+    const queryStr = createSearchParams({page: page, size: size}).toString()
 
-  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
-  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+    const moveToModify = (tno) => {
+        navigate({
+            pathname: `/todo/modify/${tno}`,
+            search: queryStr
+        })
+    }
 
-  const queryStr = createSearchParams({page,size}).toString()
+    const moveToList = () => {
+        navigate({
+            pathname: `/todo/list`,
+            search: queryStr
+        })
+    }
 
-  const moveToModify = useCallback((tno) => {
-
-    navigate({
-      pathname: `/todo/modify/${tno}`,
-      search: queryStr
-    })
-
-  },[tno, page, size])
-
-  const moveToList = useCallback(() => {
-
-    navigate({pathname:`/todo/list`, search: queryStr})
-  }, [page, size])
-
-
-  return ( 
-    <div className="text-3xl font-extrabold">
-      Todo Read Page Component {tno}
-
-      <div>
-        <button onClick={() => moveToModify(tno)}>Test Modify</button>
-
-        <button onClick={() => moveToList()}>Test List</button>
-      </div>
-      
-    </div> 
-  );
-
+    return (
+        <div className="text-3xl">
+            Todo Read Page {tno}
+            <div>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
+                <button onClick={moveToList}>test List</button>
+            </div>
+        </div>
+    );
 }
- 
+
 export default ReadPage;
