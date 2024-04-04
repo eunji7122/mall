7e33d1c0ee55fde15e852c49fa62365cdd938ef7
eunji7122 <lej7122@gmail.com>
2024-04-04 22:34:02 +0900

    Section3: API서버 통신

--- src/pages/todo/ModifyPage.js
+++ src/pages/todo/ModifyPage.js
@@ -1,12 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import {useParams} from "react-router-dom";
+import ModifyComponent from "../../components/todo/ModifyComponent";
 
-const ModifyPage = ({tno}) => {
-  return ( 
-    <div className="text-3xl font-extrabold">
-      Todo Modify Page 
-    </div> 
+const ModifyPage = () => {
+
+
+    const {tno} = useParams()
+
+
+  return (
+      <div className="p-4 w-full bg-white">
+          <div className="text-3xl font-extrabold">
+              Todo Modify Page
+          </div>
+          <ModifyComponent tno={tno}/>
+      </div>
    );
-  
+
 }
  
 export default ModifyPage;
