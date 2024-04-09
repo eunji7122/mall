4c31b6b56271b1797402f5efb869f7fa85aab75a
eunji7122 <lej7122@gmail.com>
2024-04-10 00:27:40 +0900

    Section5: 상품 API 서버 연동

---
+++ src/pages/products/ListPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListComponent from "../../components/products/ListComponent";
+
+function ListPage(props) {
+    return (
+        <div className="w-full mt-4 border border-solid border-neutral-300 shadow-md">
+            <div className="text-2xl m-4 font-extrabold">
+                Products List Page
+            </div>
+            <ListComponent/>
+        </div>
+    );
+}
+
+export default ListPage;
--- src/router/root.js
+++ src/router/root.js
@@ -1,11 +1,13 @@
 import {createBrowserRouter} from "react-router-dom";
 import {lazy, Suspense} from "react";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
 
 const Loading = <div className="bg-red-700">Loading...</div>
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+const ProductIndex = lazy(() => import("../pages/products/IndexPage"))
 
 
 
@@ -22,6 +24,11 @@ const root = createBrowserRouter([
         path: 'todo',
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children: todoRouter
+    },
+    {
+        path: 'products',
+        element: <Suspense fallback={Loading}><ProductIndex/></Suspense>,
+        children: productsRouter()
     },
 ])
 
